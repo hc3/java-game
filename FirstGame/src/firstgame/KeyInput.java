6d18c09ee5e34730f923cfc70f33187084077e21
hc3 <eliel.floyd@bol.com.br>
2018-07-27 16:13:10 -0300

    starting

--- FirstGame/src/firstgame/KeyInput.java
+++ FirstGame/src/firstgame/KeyInput.java
@@ -29,15 +29,33 @@ public class KeyInput extends KeyAdapter {
             
             if(tempObject.getId() == ID.Player) {
                 
-                if(key == KeyEvent.VK_W) tempObject.setY(tempObject.getY() -1 );
+                if(key == KeyEvent.VK_W) tempObject.setVelY(-5);
+                if(key == KeyEvent.VK_S) tempObject.setVelY(5);
+                if(key == KeyEvent.VK_D) tempObject.setVelX(5);
+                if(key == KeyEvent.VK_A) tempObject.setVelX(-5);
+                
             }
         }
-        
-        System.out.println("Key: "+key);
+
     }
 
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
+        int index = 0;
+        
+        for(GameObject object: handler.listObject) {
+            GameObject tempObject = handler.listObject.get(index);
+            
+            if(tempObject.getId() == ID.Player) {
+                
+                if(key == KeyEvent.VK_W) tempObject.setVelY(0);
+                if(key == KeyEvent.VK_S) tempObject.setVelY(0);
+                if(key == KeyEvent.VK_D) tempObject.setVelX(0);
+                if(key == KeyEvent.VK_A) tempObject.setVelX(0);
+                
+            }
+        }
+                
     }
 
 }
